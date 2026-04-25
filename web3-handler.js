bb421a2729c1eb5f3736ad7857ea09da610b791a
lumicsoft <lumicsoft@gmail.com>
2026-04-25 15:41:25 +0530

    Update web3-handler.js

--- web3-handler.js
+++ web3-handler.js
@@ -1,5 +1,5 @@
 let provider, signer, contract, usdtContract;
-const CONTRACT_ADDRESS = "0x38EAAa5996eEAb28E3323c3540880983f90d0E80"; 
+const CONTRACT_ADDRESS = "0xF1b6BEA3544c1944550045425Eb228815790a7A4"; 
 const USDT_ADDRESS = "0x3B66b1E08F55AF26c8eA14a73dA64b6bC8D799dE"; // Testnet USDT
 
 window.userData = {
